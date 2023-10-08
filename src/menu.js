f31b1c7b93fdf5aa7d5352f8a34196e521f0fda5
AndrewJamesC <130733134+AndrewJamesC@users.noreply.github.com>
2023-10-08 16:02:25 +1300

    Create hotdogs menu

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,33 @@
 import cokeImg from "./assets/coke.jpeg";
 import spriteImg from "./assets/sprite.png";
+import plainHotdogImg from "./assets/originalHotdog.jpg"
+import chilidogImg from "./assets/chiliDog.jpg"
 
 
 export const createMenu = () => {
     const divContent = document.querySelector("#content");
+    divContent.style.cssText = "width: 50vw;"
     const menuHeading = document.createElement("h1");
     menuHeading.innerText = "Menu";
+    const hotdogsHeading = document.createElement("h2");
+    hotdogsHeading.innerText = "Hotdogs";
+    const hotdogsContainer = document.createElement("div");
+    hotdogsContainer.style.cssText = "border: 2px solid black";
+    const plainHotdog = document.createElement("h3");
+    plainHotdog.innerText = "Original Hotdog";
+    const plainHotdogDescription = document.createElement("p");
+    plainHotdogDescription.innerText = "Our Classic Frankfurter starts with a top-quality all-beef frankfurter, expertly grilled to perfection which is nestled in a soft, lightly toasted bun. Customize it with your choice of traditional condiments such as ketchup, mustard, relish, and fresh, crispy onions.";
+    const orginalHotdogImg = new Image();
+    orginalHotdogImg.src = plainHotdogImg;
+    orginalHotdogImg.style.cssText = "height: 130px;"
+    const chiliDog = document.createElement("h3");
+    chiliDog.innerText = "The Classic Chili Dog";
+    const chiliDogDescription = document.createElement("p");
+    chiliDogDescription.innerText = "We start with a plump, juicy beef frankfurter, gently nestled in a soft, warm bun. Then, we ladle on our rich and hearty homemade chili, made from a secret blend of spices and ground beef, simmered to perfection. To add a touch of freshness and contrast to the savory chili, we top it off with cool, crisp diced onions and a generous sprinkle of shredded cheddar cheese."
+    const chilidogImage = new Image();
+    chilidogImage.src = chilidogImg;
+    chilidogImage.style.cssText = "height: 130px;"
+
     const drinksHeading = document.createElement("h2");
     drinksHeading.innerText = "Drinks";
     const drinksContainer = document.createElement("div");
@@ -25,6 +47,15 @@ export const createMenu = () => {
     spriteGlass.style.cssText = "height: 130px; width: 130px;"
 
     divContent.appendChild(menuHeading);
+    divContent.appendChild(hotdogsHeading);
+    divContent.appendChild(hotdogsContainer);
+    hotdogsContainer.appendChild(plainHotdog);
+    hotdogsContainer.appendChild(plainHotdogDescription);
+    hotdogsContainer.appendChild(orginalHotdogImg);
+    hotdogsContainer.appendChild(chiliDog);
+    hotdogsContainer.appendChild(chiliDogDescription);
+    hotdogsContainer.appendChild(chilidogImage);
+
     divContent.appendChild(drinksHeading);
     divContent.appendChild(drinksContainer);
     drinksContainer.appendChild(coke);
