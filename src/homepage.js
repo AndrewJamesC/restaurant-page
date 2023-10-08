f1c730b837812dc64f074428d04b75c72ec3a1aa
AndrewJamesC <130733134+AndrewJamesC@users.noreply.github.com>
2023-10-08 13:47:59 +1300

    Set homepage background image

--- src/homepage.js
+++ src/homepage.js
@@ -5,16 +5,19 @@ export const createHome = () => {
     const heading = document.createElement("h1");
     const homePara = document.createElement("p");
     const headerImg = new Image();
+    
+    document.body.style.backgroundImage = "url('../src/assets/flatten.jpeg')";
+
     headerImg.src = harryImg;
     headerImg.style.cssText = "height: auto; width: 10rem;"
     
 
     heading.textContent = "Harry's Hotdogs";
     homePara.innerText = "You're a wizard Harry! These hotdogs are amazing!!!"
-    divContent.appendChild(headerImg);
+    
     divContent.appendChild(heading); 
     divContent.appendChild(homePara);
-   
+    heading.prepend(headerImg);
    
     
 
